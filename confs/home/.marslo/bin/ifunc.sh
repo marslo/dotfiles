8407eb7c5e193884e582db046d34fe6d80060ee4
marslo <marslo.jiao@gmail.com>
2023-10-07 20:54:27 -0700

    refactoring im and ifunc

--- confs/home/.marslo/bin/ifunc.sh
+++ confs/home/.marslo/bin/ifunc.sh
@@ -7,33 +7,24 @@
 #  LastChange : 2021-01-15 22:00:55
 # =============================================================================
 
-function take() {
-  mkdir -p "$1" && cd "$1" || return
-}
+function take() { mkdir -p "$1" && cd "$1" || return; }
+function cdls() { cd "$1" && ls; }
+function cdla() { cd "$1" && la; }
+function chmv() { sudo mv "$1" "$2"; sudo chown -R "$(whoami)":"$(whoami)" "$2"; }
+function chcp() { sudo cp -r "$1" "$2"; sudo chown -R "$(whoami)":"$(whoami)" "$2"; }
+function cha() { sudo chown -R "$(whoami)":"$(whoami)" "$1"; }
+function getperm() { find "$1" -printf '%m\t%u\t%g\t%p\n'; }
+function rdiff() { rsync -rv --size-only --dry-run "$1" "$2"; }
+function rget() { route -nv get "$@"; }
+function forget() { history -d $(( $(history | tail -n 1 | ${GREP} -oP '^ \d+') - 1 )); }
+function dir755() { find . -type d -perm 0777 \( -not -path "*.git" -a -not -path "*.git/*" \) -exec sudo chmod 755 {} \; -print ; }
+function file644() { find . -type f -perm 0777 \( -not -path "*.git" -a -not -path "*.git/*" \) -exec sudo chmod 644 {} \; -print; }
+function convert2av() { ffmpeg -i "$1" -i "$2" -c copy -map 0:0 -map 1:0 -shortest -strict -2 "$3"; }
+function zh() { zipinfo "$1" | head; }
+function cleanview() { rm -rf ~/.vim/view/*; }
+function copy() { /usr/bin/pbcopy < $1; }
 
-function cdls() {
-  cd "$1" && ls
-}
-
-function cdla() {
-  cd "$1" && la
-}
-
-function chmv() {
-  sudo mv "$1" "$2"
-  sudo chown -R "$(whoami)":"$(whoami)" "$2"
-}
-
-function chcp() {
-  sudo cp -r "$1" "$2"
-  sudo chown -R "$(whoami)":"$(whoami)" "$2"
-}
-
-function cha() {
-  sudo chown -R "$(whoami)":"$(whoami)" "$1"
-}
-
-# Inspired from http://www.earthinfo.org/linux-disk-usage-sorted-by-size-and-human-readable/
+# inspired from http://www.earthinfo.org/linux-disk-usage-sorted-by-size-and-human-readable/
 function udfs {
   v='*'
   # shellcheck disable=SC2124
@@ -55,27 +46,17 @@ function mdiff() {
 }
 
 function dir() {
-  if [ 0 -eq $? ]; then
-    _p='.'
-  else
-    _p="$*"
-  fi
-
+  _p=$( [ 0 -eq $# ] && echo '.' || echo "$*" )
   find . -iname "${_p}" -print0 | xargs -r0 ${LS} -altr | awk '{print; total += $5}; END {print "total size: ", total}';
 }
 
 function dir-h() {
-  if [ 0 -eq $? ]; then
-    _p='.'
-  else
-    _p="$*"
-  fi
+  _p=$( [ 0 -eq $# ] && echo '.' || echo "$*" )
   find . -iname "${_p}" -exec ${LS} -lthrNF --color=always {} \;
   find . -iname "${_p}" -print0 | xargs -r0 du -csh| tail -n 1
 }
 
-function rcsync()
-{
+function rcsync() {
   SITE="Jira Confluence Jenkins Gitlab Artifactory Sonar Slave"
   HNAME=$( hostname | tr '[:upper:]' '[:lower:]' )
   for i in $SITE; do
@@ -97,14 +78,6 @@ function rcsync()
       popd || return
     fi
   done
-}
-
-function getperm() {
-  find "$1" -printf '%m\t%u\t%g\t%p\n'
-}
-
-function rdiff() {
-  rsync -rv --size-only --dry-run "$1" "$2"
 }
 
 function proxydefault() {
@@ -136,30 +109,6 @@ function proxydefault() {
   export no_proxy NO_PROXY
 }
 
-function rget() {
-  route -nv get "$@"
-}
-
-function forget() {
-  history -d $(( $(history | tail -n 1 | ${GREP} -oP '^ \d+') - 1 ));
-}
-
-function dir755() {
-  find . -type d -perm 0777 \( -not -path "*.git" -a -not -path "*.git/*" \) -exec sudo chmod 755 {} \; -print
-}
-
-function file644() {
-  find . -type f -perm 0777 \( -not -path "*.git" -a -not -path "*.git/*" \) -exec sudo chmod 644 {} \; -print
-}
-
-function convert2av() {
-  ffmpeg -i "$1" -i "$2" -c copy -map 0:0 -map 1:0 -shortest -strict -2 "$3"
-}
-
-function zh() {
-  zipinfo "$1" | head
-}
-
 function 256color() {
   for i in {0..255}; do
     echo -e "\e[38;05;${i}m█${i}";
@@ -167,28 +116,27 @@ function 256color() {
 }
 
 function 256colors() {
-  for fgbg in 38 48 ; do # Foreground / Background
-      for color in {0..255} ; do # Colors
-        printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
-        if [ $(( (color + 1) % 6 )) == 4 ] ; then
-            echo # New line
-        fi
-      done
-      echo # New line
+  for fgbg in 38 48 ; do                   # foreground / background
+    for color in {0..255} ; do             # colors
+      # display the color
+      printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+      # display 6 colors per lines
+      if [ $(( (color + 1) % 6 )) == 4 ] ; then
+        echo                               # new line
+      fi
+    done
+    echo                                   # new line
   done
 }
 
 function 256colorsAll() {
-  #Background
-  for clbg in {40..47} {100..107} 49 ; do
-    #Foreground
-    for clfg in {30..37} {90..97} 39 ; do
-      #Formatting
-      for attr in 0 1 2 4 5 7 ; do
-        #Print the result
+  for clbg in {40..47} {100..107} 49 ; do  # background
+    for clfg in {30..37} {90..97} 39 ; do  # foreground
+      for attr in 0 1 2 4 5 7 ; do         # formatting
+        # print the result
         echo -en "\e[${attr};${clbg};${clfg}m ^[${attr};${clbg};${clfg}m \e[0m"
       done
-      echo #Newline
+      echo                                 # new line
     done
   done
 }
@@ -239,47 +187,53 @@ gtoc() {
        -exec doctoc --github --maxlevel 3 {} \;
 }
 
-function cleanview() {
-  rm -rf ~/.vim/view/*
-}
-
-function copy() {
-  /usr/bin/pbcopy < $1
-}
-
+# /**************************************************************
+#  __      __
+#  / _|    / _|
+# | |_ ___| |_
+# |  _|_  /  _|
+# | |  / /| |
+# |_| /___|_|
+#
+# **************************************************************/
 # brew install fzf
-function fs() {
-  # vim $(/usr/local/bin/fzf --height 40% --layout=reverse --multi)
-  # fzf --bind 'enter:become(vim {})'
-  fzf --multi --bind 'enter:become(vim {+})'
-}
+
+# vim $(/usr/local/bin/fzf --height 40% --layout=reverse --multi)
+# fzf --bind 'enter:become(vim {})'
+function fs() { fzf --multi --bind 'enter:become(vim {+})'; }
 
 # fe [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
 # https://github.com/junegunn/fzf/wiki/examples#opening-files
 fe() {
+  # shellcheck disable=SC2207
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  # shellcheck disable=SC2128
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
 
 # fda - including hidden directories
 fda() {
   local dir
+  # shellcheck disable=SC2164
   dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
 }
 
 # cdp - cd to selected parent directory
 cdp() {
+  # shellcheck disable=SC2034,SC2316
   local declare dirs=()
   get_parent_dirs() {
     if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
     if [[ "${1}" == '/' ]]; then
       for _dir in "${dirs[@]}"; do echo $_dir; done
     else
+      # shellcheck disable=SC2046
       get_parent_dirs $(dirname "$1")
     fi
   }
+  # shellcheck disable=SC2155,SC2046
   local DIR=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux --tac)
   cd "$DIR" || return
 }
@@ -288,6 +242,7 @@ cdp() {
 cdf() {
   local file
   local dir
+  # shellcheck disable=SC2164
   file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
 }
 
