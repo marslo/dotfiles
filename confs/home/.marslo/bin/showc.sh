69c07280d12df4bfee1ee7715506dda753641e22
marslo <marslo.jiao@gmail.com>
2023-12-16 00:09:43 -0800

    * extract ff into bin/ff instead of system-loaded fucntion

--- confs/home/.marslo/bin/showc.sh
+++ confs/home/.marslo/bin/showc.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # credit belongs to https://github.com/ppo/bash-colors
 
-# https://github.com/ppo/bash-colors (v0.3.0)
-c() { [ $# == 0 ] && printf "\e[0m" || printf "$1" | sed 's/\(.\)/\1;/g;s/\([SDIUFNHT]\)/2\1/g;s/\([KRGYBMCW]\)/3\1/g;s/\([krgybmcw]\)/4\1/g;y/SDIUFNHTsdiufnhtKRGYBMCWkrgybmcw/12345789123457890123456701234567/;s/^\(.*\);$/\\e[\1m/g'; }
-cecho() { echo -e "$(c $1)${2}\e[0m"; }
+source "${iRCHOME}/bin/bash-color.sh"
 
 declare -A LABELS=(
   [K]="fg Black" [R]="fg Red" [G]="fg Green" [Y]="fg Yellow" [B]="fg Blue" [M]="fg Magenta" [C]="fg Cyan" [W]="fg White"
