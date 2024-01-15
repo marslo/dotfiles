298b60d80c8c79b102c447f64f646530c2143951
marslo <marslo.jiao@gmail.com>
2024-01-14 18:06:28 -0800

    using rrethy/vim-hexokinase instead of NvChad/nvim-colorizer.lua for groovy filetype lsp disable issue

--- confs/home/.config/nvim/init.lua
+++ confs/home/.config/nvim/init.lua
@@ -11,17 +11,6 @@ function _G.check_back_space()
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
--- require 'colorizer'.setup()
--- require 'colorizer'.setup {
---   filetypes = {
---     '*';
---     css = { rgb_fn = true; mode = 'background'; };
---     html = { names = true; };
---     cmp_docs = {always_update = true}
---   },
---   user_default_options = { RRGGBBAA = true, css_fn = true, css = true, tailwind = true },
---   buftypes = { "*", "!prompt", "!popup", }
--- }
-
+-- require('config/theme')
 require('config/nvim-treesitter')
 require('config/lsp')
