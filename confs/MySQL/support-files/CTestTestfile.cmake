2f253535aebaee22e046d081bf30aa548ec09909
marslo <marslo.jiao@gmail.com>
2024-01-17 01:34:37 -0800

    [1.0.0] restructure main branch for dotfiles only

--- confs/MySQL/support-files/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marslo/Tools/MySQL/mysql-5.5.28/support-files
-# Build directory: /home/marslo/Tools/MySQL/mysql-5.5.28/support-files
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
