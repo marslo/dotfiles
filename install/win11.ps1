0eac25a8a0ec257b4c54459c3f4e0b2161faadb5
marslo <marslo.jiao@gmail.com>
2024-02-23 00:48:17 -0800

    add win11.ps1 for PSReadLine Powershell Modeule; update vimrc.d for nvim/vim in powershell

--- install/win11.ps1
+++ install/win11.ps1
@@ -1,5 +1,5 @@
 # hyper-v
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+knable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
@@ -18,15 +18,45 @@ winget upgrade
 winget show Microsoft.WindowsTerminal --versions
 
 # choco
+[Net.WebRequest]::DefaultWebProxy.Credentials = [Net.CredentialCache]::DefaultCredentials
+$env:chocolateyProxyLocation = 'http://proxy.sample.com:8080'
+choco config set --name="'proxy'" --value="'http://proxy.sample.com:8080'"
+choco config set --name="'proxyBypassOnLocal'" --value="'true'"
+choco config list
+
 Get-ExecutionPolicy
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+
 choco upgrade microsoft-windows-terminal
-choco install python312
-choco install make
-choco install go
+choco install -y python312
+choco install -y make
+choco install -y go
+# C:\Users\marslo\AppData\Local\Temp\chocolatey\neovim\0.9.5\nvim-win64.zip
+choco install -y neovim
+choco install -y fzf
+choco install -y ripgrep
+choco install -y fd
+
+# powershell models
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+Install-Module PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+
+Install-Module PSWriteColor
+Install-Module WebKitDev
+Install-Module PowerShellGet -Force
 
 # vim plugin
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/.vim/autoload/plug.vim -Force
 cd "C:\Users\marslo\.vim\plugged\vim-hexokinase"
 make hexokinase
+
+# PowerToys Run
+# copy `_def` to `C:\ProgramData\Microsoft\Windows\Start Menu\Programs`
