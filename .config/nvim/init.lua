87d82e2323587b6662c3255c6f6858b498c6e8e5
marslo <marslo.jiao@gmail.com>
2024-04-09 19:38:40 -0700

    fixed persistent undo issue; add copilot plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,8 @@ vim.cmd( 'set runtimepath^=~/.vim runtimepath+=~/.vim/after' )
 vim.cmd( 'let &packpath = &runtimepath' )
 vim.cmd( 'source ~/.vimrc' )
 vim.cmd( 'autocmd TextYankPost * silent! lua vim.highlight.on_yank {on_visual=false}' )
-vim.opt.undodir = vim.fn.stdpath('config') .. '/undo'
+vim.opt.undodir = vim.fn.expand( '~/.vim/undo' )
+-- vim.opt.undodir = vim.fn.stdpath('config') .. '/undo'
 
 -- require('config/nvim-treesitter')
 -- require('config/lsp')
