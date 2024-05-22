ad531720a1bdd818b7b82a1ba3a10ea4cc412c48
marslo <marslo.jiao@gmail.com>
2024-05-22 01:10:16 -0700

    enable CopilotChat plugin for nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@
      FileName : init.lua
        Author : marslo.jiao@gmail.com
       Created : 2024-01-11 01:33:04
-   LastChange : 2024-04-17 20:54:21
+   LastChange : 2024-05-22 01:09:48
 =============================================================================
 --]]
 
@@ -15,6 +15,12 @@ vim.opt.undodir = vim.fn.expand( '~/.vim/undo' )
 -- vim.opt.undodir = vim.fn.stdpath('config') .. '/undo'
 
 require('config/nvim-treesitter')
+-- CopilotC-Nvim/CopilotChat.nvim
+require("CopilotChat").setup {
+  debug = true,
+  allow_insecure = true, -- https://github.com/deathbeam/dotfiles/blob/master/nvim/.config/nvim/lua/config/copilot.lua
+  show_folds = false,
+}
 -- require('config.snippets')
 -- require('config/lsp')
 -- require('undotree').setup()
