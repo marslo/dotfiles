0cf99dd7435d0f571aee6dfc87e6971c71105966
marslo <marslo.jiao@gmail.com>
2025-03-21 04:31:23 -0700

    chore(vimrc,ffunc): improve coc UX, weather script, and autocmd layout
    - adjust coc-settings.json: enable preselect, reorder source priorities
    - major refactor of autocmd groups and event organization
    - enable markdown-preview and vimlsp plugins
    - update various key mappings: snippets, float scroll, diagnostics, etc.
    
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,3 +25,16 @@ require("CopilotChat").setup {
 -- require('config.snippets')
 -- require('config/lsp')
 -- require('undotree').setup()
+
+--[[
+-- coc-symbol-line --
+function _G.symbol_line()
+  local curwin = vim.g.statusline_winid or 0
+  local curbuf = vim.api.nvim_win_get_buf(curwin)
+  local ok, line = pcall(vim.api.nvim_buf_get_var, curbuf, 'coc_symbol_line')
+  return ok and line or ''
+end
+vim.o.tabline    = '%!v:lua.symbol_line()'
+vim.o.statusline = '%!v:lua.symbol_line()'
+vim.o.winbar     = '%!v:lua.symbol_line()'
+--]]
