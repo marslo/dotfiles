414e62687770693215fa263df504178d5c3b3aea
marslo <marslo.jiao@gmail.com>
2025-03-25 08:28:02 -0700

    refactor(gitalias,gitcolors,vimrc.d): streamline git aliases and update color schemes
    - Simplify `ca` alias to use external script for commit all changes
    - Add new color definitions for `func`, `context`, and `stash` in `.gitcolors`
    - Change shortcut for coc-snippets-expand from `<C-l>` to `<C-S-j>` in vimrc
    
    Signed-off-by: marslo <marslo.jiao@gmail.com>

---
+++ .marslo/bin/getCommitMessage.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+# shellcheck source=/dev/null
+
+# | ENVIRONMENT       | DESCRIPTION               | DEFAULT |
+# |-------------------|---------------------------|---------|
+# | MCX_ENABLE_BODY   | is enable commit body     | false   |
+# | MCX_ENABLE_FOOTER | is enable commit footer   | false   |
+# | MCX_ENABLE_SCOPE  | is enable commit scope    | false   |
+# | MCX_SHOW_PREVIEW  | is show commit preview    | false   |
+# | MCX_SHOW_DIFF     | is show diff after commit | false   |
+# | MCX_LINT_ENABLE   | is enable commit lint     | true    |
+# | MCX_ALLOW_EMPTY   | is enable commit lint     | false   |
+
+
+set -euo pipefail
+
+source "${HOME}"/.marslo/bin/bash-color.sh
+
+types=(
+  "feat:     $(c Wdi)a new feature$(c)"
+  "fix:      $(c Wdi)a bug fix$(c)"
+  "docs:     $(c Wdi)documentation only changes$(c)"
+  "style:    $(c Wdi)changes that do not affect the meaning of the code$(c)"
+  "refactor: $(c Wdi)a code change that neither fixes a bug nor adds a feature$(c)"
+  "perf:     $(c Wdi)a code change that improves performance$(c)"
+  "test:     $(c Wdi)adding missing tests or correcting existing tests$(c)"
+  "build:    $(c Wdi)changes that affect the build system or external dependencies$(c)"
+  "ci:       $(c Wdi)changes to our CI configuration files and scripts$(c)"
+  "chore:    $(c Wdi)other changes that don't modify src or test files$(c)"
+  "revert:   $(c Wdi)reverts a previous commit$(c)"
+)
+
+function die() { echo -e "$(c Ri)ERROR$(c) : $*. exit ..." >&2; exit 1; }
+
+# @usage       : runInteractive <varname> <command string>
+# @description : executes a dynamic or piped shell command and assigns its stdout output to a variable.
+# @purpose     : in Bash, when a function is called as subshell ( `$(..)` ),
+#                the aborts action for interactive commands will in subshell  will not be captured/propagated in parent shell.
+# @applicable  : 1. capture the output of dynamic/piped commands
+#                2. detect user cancellation or failure in interactive tools
+#                3. avoid writing repetitive boilerplate error checking after each fzf/gum
+function runInteractive() {
+  local __varname=$1
+  shift
+
+  local output
+
+  # If one argument and it's a piped string → use eval
+  if [[ $# -eq 1 && "$1" == *"|"* ]]; then
+    output="$(eval "$1")" || return 130
+  else
+    output="$("$@")" || return 130
+  fi
+
+  printf -v "$__varname" "%s" "$output"
+}
+
+function getCommitMessage() {
+  selected=$( printf "%b\n" "${types[@]}" |
+              fzf --prompt="commit type: " --height=10 --border --ansi --color=fg+:#979736,hl+:#979736
+            )
+  [[ -n "${selected}" ]] || die 'no commit type selected'
+  type=$(echo "${selected}" | awk -F: '{print $1}')
+
+  read -rp "$(printf "$(c Mi)%s$(c)" 'scope (optional): ')" scope
+  [[ -n "${scope}" ]] && scope="(${scope})"
+
+  read -rp "$(printf "$(c Mi)%s$(c)" 'commit message subject: ')" subject
+  [[ -z "${subject}" ]] && die 'no commit subject entered'
+
+  if [[ 'true' = "${MCX_ENABLE_BODY:-false}" ]]; then
+    runInteractive body gum write --placeholder="commit body (optional)" || return 1
+  fi
+
+  if [[ 'true' = "${MCX_ENABLE_FOOTER:-false}" ]]; then
+    runInteractive footer gum input --placeholder="footer (optional -- i.e.: Closes #123)" || return 1
+  fi
+
+  message="${type}${scope}: ${subject}"
+  [[ -n "${body:-}"   ]] && message+=$'\n\n'"$body"
+  [[ -n "${footer:-}" ]] && message+=$'\n\n'"$footer"
+
+  echo "${message}"
+}
+
+# vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=sh
