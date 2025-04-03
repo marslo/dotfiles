410af5c72bfb4cfb17f0c671c6db1a2046f7825c
marslo <marslo.jiao@gmail.com>
2025-04-03 03:09:52 -0700

    refactor(hex2rgba): streamline the duplicated code in 'parseHex' and 'parseBackground'; add screenshots for rgba/hex converter
    - disable the subject max length in commitlintrc
    - add screenshots for the hex2rgba and rgba2hex converter
    - fix the readline issue in `git mcx`
    
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,2 +1,7 @@
 // CommonJS Syntax (most common)
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0]                    // disable the header-max-length rule
+  }
+};
