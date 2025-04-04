fb63b78cebc305732146eb21ba18834ecc178be4
marslo <marslo.jiao@gmail.com>
2025-04-04 00:41:28 -0700

    feat(nvim_mini,ansi2hex,pandoc): add nvim_minimal for nvim debug; update ansi2hex; add html and css for pandoc
    - update the ansi2hex, to support multiple input ansi colors
    - fixed and enhancement fdInRC with parallel processing and exclusion patterns ( xargs -P )
    - update document title, and enalbe the alert format in pandoc header template
    
    Signed-off-by: marslo <marslo.jiao@gmail.com>

---
+++ .config/nvim_minimal/init.lua
@@ -0,0 +1,43 @@
+--[[
+=============================================================================
+     FileName : init.lua
+       Author : marslo.jiao@gmail.com
+      Created : 2024-01-11 01:33:04
+   LastChange : 2024-05-22 01:09:48
+=============================================================================
+--]]
+
+vim.cmd( 'set runtimepath^=~/.vim runtimepath+=~/.vim/after' )
+vim.cmd( 'let &packpath = &runtimepath' )
+vim.cmd( 'source ~/.vimrc' )
+vim.cmd( 'autocmd TextYankPost * silent! lua vim.highlight.on_yank {on_visual=false}' )
+vim.opt.undodir = vim.fn.expand( '~/.vim/undo' )
+-- vim.opt.undodir = vim.fn.stdpath('config') .. '/undo'
+
+--[[
+require('config/nvim-treesitter')
+-- CopilotC-Nvim/CopilotChat.nvim
+require("CopilotChat").setup {
+  debug = true,
+  allow_insecure = true, -- https://github.com/deathbeam/dotfiles/blob/master/nvim/.config/nvim/lua/config/copilot.lua
+  show_folds = false
+}
+--]]
+
+-- require("ibl").setup()
+-- require('config.snippets')
+-- require('config/lsp')
+-- require('undotree').setup()
+
+--[[
+-- coc-symbol-line --
+function _G.symbol_line()
+  local curwin = vim.g.statusline_winid or 0
+  local curbuf = vim.api.nvim_win_get_buf(curwin)
+  local ok, line = pcall(vim.api.nvim_buf_get_var, curbuf, 'coc_symbol_line')
+  return ok and line or ''
+end
+vim.o.tabline    = '%!v:lua.symbol_line()'
+vim.o.statusline = '%!v:lua.symbol_line()'
+vim.o.winbar     = '%!v:lua.symbol_line()'
+--]]
