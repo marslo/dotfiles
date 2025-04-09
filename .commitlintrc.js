d1bc24504cb277ba3d95d7524b14689d82a156bc
marslo <marslo.jiao@gmail.com>
2025-04-09 00:09:56 -0700

    feat(env,ffunc/help): enable `MCX_ENABLE_BODY` into env file; seperate the ffunc/help() into individual bin file ( ~/.marslo/bin/ ); setup the help in ffunc as alias to replace the built-in help command
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,6 +2,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [0]                    // disable the header-max-length rule
+    'header-max-length': [0],                   // disable the header-max-length rule
+    'body-max-line-length': [0],                // disable the body-max-line-length rule
   }
 };
