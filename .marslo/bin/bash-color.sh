fd1436b226e69279ef3b654ce21ae8d5aeef9a8e
marslo <marslo.jiao@gmail.com>
2025-04-10 01:56:18 -0700

    chore(colors): merge solarized color into icolors
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- .marslo/bin/bash-color.sh
+++ .marslo/bin/bash-color.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# references:
-# - credit [ppo/bash-colors v3.0](https://github.com/ppo/bash-colors/blob/master/bash-colors.sh)
+# credit: [ppo/bash-colors v3.0](https://github.com/ppo/bash-colors/blob/master/bash-colors.sh)
+# author: @ppo
+
 #  ┌───────┬────────────────┬─────────────────┐   ┌───────┬─────────────────┬───────┐
-#  │ Fg/Bg │ Color          │ Octal           │   │ Code  │ Style           │ Octal │
+#  │ FG/BG │ COLOR          │ OCTAL           │   │ CODE  │ STYLE           │ OCTAL │
 #  ├───────┼────────────────┼─────────────────┤   ├───────┼─────────────────┼───────┤
 #  │  K/k  │ Black          │ \e[ + 3/4  + 0m │   │  s/S  │ Bold (strong)   │ \e[1m │
 #  │  R/r  │ Red            │ \e[ + 3/4  + 1m │   │  d/D  │ Dim             │ \e[2m │
@@ -17,8 +18,10 @@
 #  │  High intensity        │ \e[ + 9/10 + *m │   │   0   │ Reset           │ \e[0m │
 #  └────────────────────────┴─────────────────┘   └───────┴─────────────────┴───────┘
 #                                                  Uppercase = Reset a style: \e[2*m
+
 # shellcheck disable=SC2015,SC2059
 c() { [ $# == 0 ] && printf "\e[0m" || printf "$1" | sed 's/\(.\)/\1;/g;s/\([SDIUFNHT]\)/2\1/g;s/\([KRGYBMCW]\)/3\1/g;s/\([krgybmcw]\)/4\1/g;y/SDIUFNHTsdiufnhtKRGYBMCWkrgybmcw/12345789123457890123456701234567/;s/^\(.*\);$/\\e[\1m/g'; }
 # shellcheck disable=SC2086
 cecho() { echo -e "$(c $1)${2}\e[0m"; }
 
+# vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=sh:
