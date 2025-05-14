6be334c285f89b521d6035afed0766eb4f238b9e
marslo <marslo.jiao@gmail.com>
2025-05-13 20:37:29 -0700

    chore(commitlintrc): update commitlint rules
    - allow empty commits with git amend in .gitalias
    
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- .jira.commitlintrc.js
+++ .jira.commitlintrc.js
@@ -2,6 +2,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [0],
+    'header-max-length': [0],                   // disable the header-max-length rule
+    'body-max-line-length': [0],                // disable the body-max-line-length rule
+    'subject-case': [0],                        // disable the subject-case rule
   },
 };
