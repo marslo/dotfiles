27bef1c9bb87d8b5e8fbdc765e4aaf187ba7543a
marslo <marslo.jiao@gmail.com>
2026-02-27 23:30:02 -0800

    feat(completion,gh.arsenal,gh-ops): refactor gh-ops by using `gh pr list --json` instead of `gh api ..`; add bash completion for gh arsenal tools
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- .marslo/bash_completion.d/gitalias.sh
+++ .marslo/bash_completion.d/gitalias.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2207
+#=============================================================================
+#     FileName : gitalias.sh
+#       Author : marslo
+#      Created : 2025-12-11 21:28:56
+#   LastChange : 2026-02-27 23:31:53
+#=============================================================================
 
 function _git_rob() {
   _git_checkout
@@ -15,5 +22,50 @@ function _git_pl() {
 function _git_del() {
   _git_checkout
 }
+
+function _git_mcx() {
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+  local has_dash_dash=false
+  for (( i=1; i < COMP_CWORD; i++ )); do
+    if [[ "${COMP_WORDS[i]}" == "--" ]]; then
+      has_dash_dash=true
+      break
+    fi
+  done
+
+  if ${has_dash_dash}; then
+    if [[ ${cur} == -* ]] || [[ -z ${cur} ]]; then
+        COMPREPLY=( $(compgen -W "--help" -- "${cur}") )
+    fi
+    return 0
+  fi
+
+  opts="--body --no-body --footer --no-footer --diff --no-diff \
+        --preview --no-preview --scope --no-scope --lint --no-lint \
+        --global-lintrc --no-global-lintrc --lintrc \
+        --breaking --breaking-change -h"
+
+  case "${prev}" in
+    --lintrc)
+        COMPREPLY=( $(compgen -f -- "${cur}") )
+        return 0
+        ;;
+  esac
+
+  if [[ ${cur} == -* ]]; then
+    COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+    return 0
+  fi
+}
+
+function _git_ca() {
+  _git_mcx
+}
+
+complete -F _git_mcx git-mcx
 
 # vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=sh:
